0e460480e07eaac226e680eeb8139c4b0762ff09
kajal-b17 <kajalbarad1011@gmail.com>
2025-02-04 10:19:05 +0530

    [ADD]|ui_setup(Basic field_views)

---
+++ ak_library_management/models/library_book_category.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields
+class LibraryBookCategory(models.Model):
+    _name = 'library.book.category'
+    _description = "This is the books  category class for managing the books"
+
+    name=fields.Char(string='Category')
+
