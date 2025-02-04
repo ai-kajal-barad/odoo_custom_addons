5f1c99e030b048a59a514ce6bcfc5f4dfcef7acf
kajal-b17 <kajalbarad1011@gmail.com>
2025-02-04 10:27:42 +0530

    [ADD]|Basic Relational Fields

---
+++ ak_library_management_system/models/library_book_category.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields
+class LibraryBookCategory(models.Model):
+    _name = 'library.book.category'
+    _description = "This is the books  category class for managing the books"
+
+    name=fields.Char(string='Category')
+    tags_ids=fields.Many2many('library.book.tag', string='Book Tag')
+    #tags_id=fields.Many2one('library.book.tag', string='Book Tag')
+
+
