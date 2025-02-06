16375fb72805e741a3b62fa37894f0a936ab04be
kajal-b17 <kajalbarad1011@gmail.com>
2025-02-06 10:58:14 +0530

    [DELETE]

--- ak_library_management_system/models/library_book_tag.py
+++
@@ -1,8 +0,0 @@
-#-*- coding: utf-8 -*-
-from odoo import models, fields
-
-class LibraryBookTag(models.Model):
-    _name = 'library.book.tag'
-    _description = "This is the books class for managing the books"
-
-    name=fields.Char(string='Book Tags')
