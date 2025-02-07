035ac2937cc9a76337f949a1bef38d4d7a7724f4
kajal-b17 <kajalbarad1011@gmail.com>
2025-02-07 16:18:21 +0530

    [ADD]Inheritance and customization in library management

---
+++ ak_library_management_inheritance/models/library_book.py
@@ -0,0 +1,28 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields
+
+class LibraryBook(models.Model):
+    """This class basically contains Book related informations
+	like name,authors and publication date."""
+    _name = 'library.book'
+    _description = "This is the books class for managing the books"
+
+    name=fields.Char(string='Book Title')
+    author=fields.Char(string='Author Name')
+    isbn=fields.Char(string='ISBN Number')
+    category_id=fields.Many2one(comodel_name='library.book.category',string='Book Category')
+    publication_date=fields.Date(string='Date of Publication')
+    description=fields.Text(string=' Book Summary')
+    state=fields.Selection([('available','Available'),('borrowed','Borrowed')],string='Book Availability',tracking=True,default="available")
+    library_id = fields.Many2one('library.library', string='Library')
+    tags_ids = fields.Many2many(comodel_name='library.book.tag',related='category_id.tags_ids',string='Tags')
+
+    def action_change_state(self):
+        """ This method for change
+    	state to borrowed"""
+        self.write({'state': 'borrowed'})
+
+    def action_state_available(self):
+        """This method for change
+    	state to available"""
+        self.write({'state':'available'})
