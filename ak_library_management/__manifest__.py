611c26613d9d0b7014af777bb58d1f09c5a79be5
kajal-b17 <kajalbarad1011@gmail.com>
2025-02-19 16:40:13 +0530

    [UPDATE]

---
+++ ak_library_management/__manifest__.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+
+{
+    'name': 'Library Management',
+    'version': '18.0.1.0.0',
+    'summary': 'library management system',
+    #'sequence': 10,
+    'description': """
+Its Basic Library management system""",
+    'author':'Kajal Barad',
+    'category': 'Sales/Sales',
+    'website': 'https://www.aktivsoftware.com',
+    'depends': ['web','product','sale_management','sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu_names.xml',
+        'views/library_book_views.xml',
+        'views/library_member_views.xml',
+        'views/library_book_category_views.xml',
+        'views/library_book_tag_views.xml',
+        'views/library_library_views.xml',
+        ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ ak_library_management/models/library_member.py
@@ -0,0 +1,25 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class LibraryMember(models.Model):
+    """This class have members details like name
+    contact details and membership date."""
+    _name = 'library.member'
+    _description = ("This is the member class for managing "
+                    "the members")
+
+    name=fields.Char(string='Member Name')
+    email=fields.Char(string='Email ID')
+    phone=fields.Char(string='Contact Number')
+    membership_date=fields.Date(string='Membership Start Date')
+
+    def action_view_borrow_book(self):
+        self.ensure_one()
+        return {
+            'name':'Borrowed Book',
+            'type':'ir.actions.act_window',
+            'res_model':'library.library',
+            'view_mode':'list,form',
+            'domain':[('member_id','=',self.id)],
+            'context':{'default_member_id':self.id}
+        }
