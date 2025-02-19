21bac6636171930fdf7091487c69bb0dc1df9b08
kajal-b17 <kajalbarad1011@gmail.com>
2025-02-19 15:24:55 +0530

    [UPDATE]

---
+++ ak_library_management/models/library_book_tag.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields
+
+class LibraryBookTag(models.Model):
+    """This class contain book Tags."""
+    _name = 'library.book.tag'
+    _description = "This is the books class for managing the books"
+
+    name=fields.Char(string='Book Tags')
---
+++ ak_library_management/models/library_library.py
@@ -0,0 +1,32 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class Library(models.Model):
+    """This class contain library related details."""
+    _name = 'library.library'
+    _description = "This is the library class."
+
+    name=fields.Char(string='Book Title')
+    location=fields.Char(string='Location')
+    capacity=fields.Integer(string='Capacity')
+    notes=fields.Text(string=' notes')
+    product_ids = fields.Many2many(string='Products',comodel_name='product.template')
+    member_id=fields.Many2one(comodel_name='library.member')
+    borrow_book_count=fields.Integer( string="Book Count", compute='_compute_book_count')
+
+    @api.depends('product_ids.state')
+    def _compute_book_count(self):
+        """compute method for count books"""
+        for rec in self:
+            rec.borrow_book_count=len(rec.product_ids.filtered(
+                lambda p: p.state == 'borrowed'))
+
+    def action_view_borrow_book(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Borrowed Books List',
+            'res_model': 'product.template',
+            'view_mode': 'list,form',
+            'domain': [('state', '=', 'borrowed'), ('id', 'in', self.product_ids.ids)]
+        }
