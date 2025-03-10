3963b949d487d937e3d1e3f783138af22ade3cd7
kajal-b17 <kajalbarad1011@gmail.com>
2025-03-10 14:04:29 +0530

    [DEL]Method Overriding

--- ak_library_management/models/res_users.py
+++
@@ -1,8 +0,0 @@
-#-*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    is_manager = fields.Boolean(string="Is Manager", help="Allows user to approve/reject sale orders.")
