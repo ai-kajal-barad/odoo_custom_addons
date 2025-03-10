42a73d68de1308436f73f996e9af175558dbaef1
kajal-b17 <kajalbarad1011@gmail.com>
2025-03-10 11:32:27 +0530

    [DEL]Constrains, Activity and notification

--- ak_library_management/models/__init__.py
+++
@@ -1,6 +0,0 @@
-#-*- coding: utf-8 -*-
-from . import library_library
-from . import product_template
-from . import bulk_book
-from . import res_partner
-from . import borrow_transaction_history
--- ak_library_management/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    not_trust_worthy = fields.Boolean(string='Not Trust Worthy')
-    is_member = fields.Boolean(string='Is Member')
